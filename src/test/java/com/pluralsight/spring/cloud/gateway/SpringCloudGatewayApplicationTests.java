4fc54b8f3f098bfab3feed76c6d0e67dd0aa950a
mhoffman <mike@michaelhoffmaninc.com>
2023-08-11 14:32:06 -0500

    Updated with routing

---
+++ src/test/java/com/pluralsight/spring/cloud/gateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.spring.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
